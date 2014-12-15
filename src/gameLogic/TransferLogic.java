415394dc02d63994884ed03fe4dd521011cc83ee
Menno <mennoo1996@outlook.com>
2014-12-15 12:04:12 +0100

    Error in transferlogic weg

--- src/gameLogic/TransferLogic.java
+++ src/gameLogic/TransferLogic.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TransferLogic {
 	
-	public static requestTransfer(Player player, double bid, Library library) {
+	public static void requestTransfer(Player player, double bid, Library library) {
 		Team team = library.getTeamForName(player.getTeam());
 		ArrayList<Player> playerlist = new ArrayList<Player>();
 		for (int i=0;i<team.getTeam().size();i++) {
@@ -13,7 +13,7 @@ public class TransferLogic {
 			}
 		}
 		
-		int[] ratings = new int[]
+		int[] ratings = new int[5];
 		for (int i=0;i<playerlist.size();i++) {
 			
 		}
