9595939fcfce4dd2837266a98fb456bb11769091
Menno <mennoo1996@outlook.com>
2015-01-13 20:50:34 +0100

    geen 2+ gele kaarten meer voor een speler

--- src/gameLogic/GameLogic.java
+++ src/gameLogic/GameLogic.java
@@ -134,9 +134,19 @@ public class GameLogic {
 	public static ArrayList<Player> getYellowCards (int amount, Team t) {
 		int random =0;
 		ArrayList<Player> playersWithYellow = new ArrayList<Player>();
+		ArrayList<Player> currentTeam = new ArrayList<Player>();
+		Player[] array = new Player[11];
+		for (int i=0;i<11;i++) {
+			currentTeam.add(array[i]);
+		}
 		for (int i=0;i<amount;i++) {
-			random = GameLogic.randomGenerator(0, 10);
+			int randomcounter = 10;
+			random = GameLogic.randomGenerator(0,randomcounter);
+			
 			playersWithYellow.add(t.getPositions().getPositionArray()[random]);
+			currentTeam.remove(t.getPositions().getPositionArray()[random]);
+			randomcounter--;
+			
 		} return playersWithYellow;
 	}
 	/** This method returns the players that scored the goals
